99af79fe597e81460518aa64b6ad193c948b64e2
Md. Touhidul Islam <touhidul.islam@tigerit.com>
2025-02-17 10:13:19 +0600

    Add customer logo slider

---
+++ scripts/slickCustomer.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.customer-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
